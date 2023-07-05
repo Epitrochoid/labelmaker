9ec99aa9ab839213daf08f5a6a61adac8ce58c9a
Mabry Cervin <mabry.cervin@gmail.com>
2023-07-04 21:46:57 -0400

    Pulls in example code to test out eframe::egui

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-#[derive(Debug)]
-struct LabelmakerArgs {
-    command: std::path::PathBuf
-}
+use eframe::egui;
 
-fn main() {
+fn main() -> Result<(), eframe::Error> {
     let args = match parse_args() {
         Ok(v) => v,
         Err(e) => {
@@ -12,7 +9,24 @@ fn main() {
         }
     };
     println!("{:?}", args);
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Box::<Labelmaker>::default()),
+    )
 }
+
+
+#[derive(Debug)]
+struct LabelmakerArgs {
+    command: std::path::PathBuf
+}
+
 
 fn parse_args() -> Result<LabelmakerArgs, pico_args::Error> {
     let mut raw_args = pico_args::Arguments::from_env();
@@ -26,4 +40,36 @@ fn parse_args() -> Result<LabelmakerArgs, pico_args::Error> {
 
 fn parse_path(s: &std::ffi::OsStr) -> Result<std::path::PathBuf, &'static str> {
     Ok(s.into())
+}
+
+struct Labelmaker {
+    name: String,
+    age: u32,
+}
+
+impl Default for Labelmaker {
+    fn default() -> Self {
+        Self {
+            name: "".to_owned(),
+            age: 0,
+        }
+    }
+}
+
+impl eframe::App for Labelmaker {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
 }
